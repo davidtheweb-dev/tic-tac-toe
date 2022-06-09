0fba6969632aa3e901d38183ec7e2a19f1b8593e
davidheweb <davidhermosilladuro@gmail.com>
2022-06-10 00:13:54 +0200

    Adding JavaScript logic
    - Showing and hiding the config modal (overlay)
    - Handling form submission with javascript
    - Validating user input with javascript
    - Storing and managing submitted data
    - Adding logic for starting a game
    - Managing game rounds and field selection
    - Tracking selected fields on the game board
    - Checking for a winner or draw
    - Ending the game and adding restart logic

---
+++ scripts/config.js
@@ -0,0 +1,34 @@
+function openPlayerConfig(event) {
+  editedPlayer = +event.target.dataset.playerid;
+  playerConfigOverlayElement.style.display = "block";
+  backdropElement.style.display = "block";
+}
+
+function closePlayerConfig() {
+  playerConfigOverlayElement.style.display = "none";
+  backdropElement.style.display = "none";
+  formLabelInputDiv.classList.remove("error");
+  errorsOutputElement.textContent = "";
+  formInput.value = "";
+}
+
+function savePlayerConfig(event) {
+  event.preventDefault();
+  const formData = new FormData(event.target);
+  const enteredPlayername = formData.get("playername").trim();
+
+  if (!enteredPlayername) {
+    formLabelInputDiv.classList.add("error");
+    errorsOutputElement.textContent = "Please enter a valid name!";
+    return;
+  }
+
+  const updatedPlayerDataElement = document.getElementById(
+    "player-" + editedPlayer + "-data"
+  );
+  updatedPlayerDataElement.children[1].textContent = enteredPlayername;
+
+  players[editedPlayer - 1].name = enteredPlayername;
+
+  closePlayerConfig();
+}
